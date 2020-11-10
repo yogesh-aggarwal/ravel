c6ac80c62277ba4b5fcf9c4417bd33a35b592733
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-11-10 10:14:14 +0530

    Added user route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { ArticleComponent } from "./routes/article/article.component";
 import { HomeComponent } from "./routes/home/home.component";
+import { UserComponent } from "./routes/user/user.component";
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "article/:id", component: ArticleComponent },
+  { path: "user/:id", component: UserComponent },
 ];
 
 @NgModule({
--- src/app/routes/user/user.component.ts
+++ src/app/routes/user/user.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.scss']
+  selector: "app-user",
+  templateUrl: "./user.component.html",
+  styleUrls: ["./user.component.scss"],
 })
 export class UserComponent implements OnInit {
+  id: string;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params["id"];
   }
-
 }
