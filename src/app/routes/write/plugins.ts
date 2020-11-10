8ce11546131170e2305c7d1e9e67253133a79a37
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-11-10 16:41:36 +0530

    Added EditorJS

---
+++ src/app/routes/write/plugins.ts
@@ -0,0 +1,47 @@
+import Header from "editorjs-header-with-anchor";
+import Paragraph from "editorjs-paragraph-with-alignment";
+import List from "@editorjs/list";
+import Checklist from "@editorjs/checklist";
+import Quote from "@editorjs/quote";
+import Warning from "@editorjs/warning";
+import Delimiter from "@editorjs/delimiter";
+import Alert from "editorjs-alert";
+import ImageTool from "@editorjs/image";
+import LinkTool from "@editorjs/link";
+import AttachesTool from "@editorjs/attaches";
+import Embed from "@editorjs/embed";
+import SocialPost from "editorjs-social-post-plugin";
+import Table from "@editorjs/table";
+import CodeTool from "@editorjs/code";
+import Marker from "@editorjs/marker";
+import InlineCode from "@editorjs/inline-code";
+import Underline from "@editorjs/underline";
+import TextSpolier from "editorjs-inline-spoiler-tool";
+import DragDrop from "editorjs-drag-drop";
+import Undo from "editorjs-undo";
+import Gist from "editorjs-github-gist-plugin";
+
+export {
+  Header,
+  Paragraph,
+  List,
+  Checklist,
+  Quote,
+  Warning,
+  Delimiter,
+  Alert,
+  ImageTool,
+  LinkTool,
+  AttachesTool,
+  Embed,
+  SocialPost,
+  Table,
+  CodeTool,
+  Marker,
+  InlineCode,
+  Underline,
+  TextSpolier,
+  DragDrop,
+  Undo,
+  Gist,
+};
--- src/app/routes/write/write.component.ts
+++ src/app/routes/write/write.component.ts
@@ -1,15 +1,109 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import EditorJS from "@editorjs/editorjs";
+import * as Plugins from "./plugins";
 
 @Component({
-  selector: 'app-write',
-  templateUrl: './write.component.html',
-  styleUrls: ['./write.component.scss']
+  selector: "app-write",
+  templateUrl: "./write.component.html",
+  styleUrls: ["./write.component.scss"],
 })
 export class WriteComponent implements OnInit {
+  editor = new EditorJS({
+    holder: "editorjs",
+    tools: {
+      header: {
+        class: Plugins.Header,
+        shortcut: "CMD+SHIFT+H",
+      },
+      paragraph: {
+        class: Plugins.Paragraph,
+        inlineToolbar: true,
+      },
+      list: Plugins.List,
+      checklist: {
+        class: Plugins.Checklist,
+        inlineToolbar: true,
+      },
+      quote: {
+        class: Plugins.Quote,
+        inlineToolbar: true,
+        shortcut: "CMD+SHIFT+O",
+        config: {
+          quotePlaceholder: "Enter a quote",
+          captionPlaceholder: "Quote's author",
+        },
+      },
+      warning: {
+        class: Plugins.Warning,
+        inlineToolbar: true,
+        shortcut: "CMD+SHIFT+W",
+        config: {
+          titlePlaceholder: "Title",
+          messagePlaceholder: "Message",
+        },
+      },
+      delimiter: Plugins.Delimiter,
+      alert: Plugins.Alert,
+      image: {
+        class: Plugins.ImageTool,
+        config: {
+          endpoints: {
+            byFile: "http://localhost:8008/uploadFile", // Your backend file uploader endpoint
+            byUrl: "http://localhost:8008/fetchUrl", // Your endpoint that provides uploading by Url
+          },
+        },
+      },
+      linkTool: {
+        class: Plugins.LinkTool,
+        config: {
+          endpoint: "http://localhost:8008/fetchUrl", // Your backend endpoint for url data fetching
+        },
+      },
+      attaches: {
+        class: Plugins.AttachesTool,
+        config: {
+          endpoint: "http://localhost:8008/uploadFile",
+        },
+      },
+      embed: {
+        class: Plugins.Embed,
+        config: {
+          services: {
+            youtube: true,
+            coub: true,
+            github: true,
+          },
+        },
+      },
+      socialPost: Plugins.SocialPost,
+      table: {
+        class: Plugins.Table,
+        inlineToolbar: true,
+        config: {
+          rows: 2,
+          cols: 3,
+        },
+      },
+      code: Plugins.CodeTool,
+      Marker: {
+        class: Plugins.Marker,
+        shortcut: "CMD+SHIFT+M",
+      },
+      inlineCode: {
+        class: Plugins.InlineCode,
+        shortcut: "CMD+SHIFT+M",
+      },
+      underline: Plugins.Underline,
+      TextSpolier: Plugins.TextSpolier,
+      gist: Plugins.Gist,
+    },
+    onReady: () => {
+      new Plugins.DragDrop(this.editor);
+      new Plugins.Undo({ editor: this.editor });
+    },
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
